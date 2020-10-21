c96ca25f9dc3e4cbee09ca30a9b3854fc298e9f6
Orest Yakymiv <akimivorest95@gmail.com>
2020-10-21 12:15:33 +0300

    first branch_02 commit

--- Project_1/Project_1.cpp
+++ Project_1/Project_1.cpp
@@ -6,8 +6,8 @@ int main()
 	double z1; // результат обчислення 1-го виразу
 	// double z2; // результат обчислення 2-го виразу
 	cout << "x = "; cin >> x;
-	z1 = 2 * x;
-	// z2 = x + x;
+	//z1 = 2 * x;
+	 z2 = x + x;
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
 	// cout << "z2 = " << z2 << endl;
